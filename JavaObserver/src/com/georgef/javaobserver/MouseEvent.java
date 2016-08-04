8d1de581f9149cfa4a2c1769187b88d5a36149eb
George Fang <georgef@George-Fangs-MacBook-Pro.local>
2016-08-04 11:44:01 -0700

    removed first draft

--- JavaObserver/src/com/georgef/javaobserver/MouseEvent.java
+++
@@ -1,9 +0,0 @@
-package com.georgef.javaobserver;
-
-/**
- * Created by georgef on 5/25/16.
- */
-public class MouseEvent
-{
-
-}
--- JavaObserver/src/com/georgef/javaobserver/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.georgef.javaobserver;
-
-/**
- * Created by georgef on 5/25/16.
- */
-public interface Observer
-{
-    void update( Subject changedSubject );
-}
